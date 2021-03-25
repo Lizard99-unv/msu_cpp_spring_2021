dc1a08cfad0d4378a59a620a06c36a4c18f12a5a
Vlad <vladlen-1999@bk.ru>
2021-03-25 19:28:59 +0300

    1

---
+++ 01/allocator.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <cassert>
+
+class Allocator
+{
+private:
+    char* ptr = nullptr;
+    int offset = 0;
+    int max;
+public:
+    void makeAllocator(size_t maxSize){
+        if (ptr != nullptr){
+            delete[] ptr;
+            offset = 0;
+        }
+        max = maxSize;
+        ptr = new char [maxSize];
+    }
+    char* alloc(size_t size){
+        if (ptr == nullptr){
+            return nullptr;
+        }
+        if (max < (offset + static_cast<int>(size))){
+            return nullptr;
+        }
+        char* block = ptr;
+        block += offset;
+        offset += size;
+        return block;
+    }
+    void reset(){
+        offset = 0;
+    }
+    ~Allocator(){
+        if (ptr != nullptr ){
+            delete[] ptr;
+        }
+    }
+};
+
+void DefaultWorkTest()
+{
+    Allocator A;
+    A.makeAllocator(5);
+    char *testPtr = A.alloc(3);
+    assert(testPtr != nullptr);
+}
+
+void LimitTest()
+{
+    Allocator A;
+    A.makeAllocator(5);
+    A.alloc(3);
+    char *testPtr = A.alloc(3);
+    assert(testPtr == nullptr);
+}
+
+void RestartTest()
+{
+    Allocator A;
+    A.makeAllocator(5);
+    A.makeAllocator(8);
+    char *testPtr = A.alloc(6);
+    assert(testPtr != nullptr);
+}
+
+void BeforeTest()
+{
+    Allocator A;
+    char *testPtr = A.alloc(6);
+    A.makeAllocator(5);
+    assert(testPtr == nullptr);
+}
+
+int main()
+{
+    DefaultWorkTest();
+    LimitTest();
+    RestartTest();
+    BeforeTest();
+
+    std::cout << "Success!\n";
+
+    return 0;
+}
