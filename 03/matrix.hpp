775b853c0bbdf991bca85b7c460c1e46282b8c29
Vlad <vladlen-1999@bk.ru>
2021-04-08 03:27:30 +0300

    3

---
+++ 03/matrix.hpp
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+
+
+class Matrix
+{
+    class ProxyRow
+    {
+    private:
+        int32_t *data_;
+        size_t size;
+    public:
+        ProxyRow();
+        ProxyRow(size_t j);
+        int getSize();
+        ProxyRow& operator= (ProxyRow &prox);
+        int32_t& operator[](size_t j);
+        ~ProxyRow();
+    };
+public:
+    Matrix(size_t i, size_t j);
+    int getRow();
+    int getCol();
+    ProxyRow& operator[](size_t i);
+    Matrix& operator *= (int32_t num);
+    Matrix& operator += (const Matrix& matrix);
+    bool operator == (const Matrix& matrix);
+    bool operator != (const Matrix& matrix);
+    friend std::ostream& operator<< (std::ostream &out, const Matrix &matrix);
+    ~Matrix();
+private:
+    ProxyRow *rows_;
+    size_t row_numbers;
+    size_t col_numbers;
+};
+
+std::ostream& operator<< (std::ostream &out, const Matrix &matrix);
