c3f09512d66b95c9d5c525e88fbbd4742aec05d3
Vlad <vladlen-1999@bk.ru>
2021-04-08 03:23:46 +0300

    2

--- 02/test.cpp
+++ 02/test.cpp
@@ -30,16 +30,29 @@ void DefaultTest()
     assert(parser.parse(str) == "Start String: abcdf5fas Number: 515 String: fasf Number: 18446744073709551615 String: 18446744073709551616 finish");
 }
 
+void EmptyStringAndSpacesTest()
+{
+    TokenParser parser;
+    parser.SetStartCallback(&StartFunc);
+    parser.SetEndCallback(&FinishFunc);
+    parser.SetStringTokenCallback(&StringFunc);
+    parser.SetDigitTokenCallback(&IntFunc);
+    string str = "";
+    string str2 = "                     ";
+    assert(parser.parse(str) == "Start finish");
+    assert(parser.parse(str2) == "Start finish");
+}
+
 void EmptyTest()
 {
-	TokenParser parser;
+    TokenParser parser;
     string str = "abcdf5fas\t  515\t fasf\t\t\n\n\n\n\n\n";
     assert(parser.parse(str) == "");
 }
 
 void IntTest()
 {
-	TokenParser parser;
+    TokenParser parser;
     parser.SetDigitTokenCallback(&IntFunc);
     string str = "abcdf5fas\t  515\t fasf\t\t\n5\n\n3\n\n\n";
     assert(parser.parse(str) == "Number: 515 Number: 5 Number: 3 ");
@@ -47,7 +60,7 @@ void IntTest()
 
 void StringTest()
 {
-	TokenParser parser;
+    TokenParser parser;
     parser.SetStringTokenCallback(&StringFunc);
     string str = "abcdf5fas\t  515\t fasf\t 5\t\n\na\n\n\n b\n";
     assert(parser.parse(str) == "String: abcdf5fas String: fasf String: a String: b ");
@@ -55,7 +68,7 @@ void StringTest()
 
 void StartFinishTest()
 {
-	TokenParser parser;
+    TokenParser parser;
     parser.SetStartCallback(&StartFunc);
     parser.SetEndCallback(&FinishFunc);
     string str = "abcdf5fas\t  515\t fasf\t\t\n\n\n\n\n\n";
@@ -64,11 +77,12 @@ void StartFinishTest()
 
 int main(int argc, char* argv[])
 {
-	DefaultTest();
+    DefaultTest();
+    EmptyStringAndSpacesTest();
     EmptyTest();
     IntTest();
     StringTest();
     StartFinishTest();
-	cout << "Success!" << endl;
-	return 0;
+    cout << "Success!" << endl;
+    return 0;
 }
