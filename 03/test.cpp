e1f1bbbca0d2453f9bf408552f896233cef1d834
Vlad <vladlen-1999@bk.ru>
2021-04-20 17:35:43 +0300

    3

--- 03/test.cpp
+++ 03/test.cpp
@@ -1,28 +1,72 @@
 #include <cassert>
 #include "matrix.hpp"
 
+void AllFunctionsTest(){
+    Matrix m(5, 3);
+    assert(m.getRow() == 5);
+    assert(m.getCol() == 3);
+    m[1][2] = 7;
+    assert(m[1][2] == 7);
+    double x = m[4][1];
+    std::cout << "Before multiplication: " << std::endl << m << std::endl;
+    m *= 3;
+    std::cout << "After: " << std::endl << m << std::endl;
+    Matrix m2(2, 2);
+    Matrix m3(2, 2);
+    std::cout << "Second matrix: " << std::endl << m2 << std::endl;
+    std::cout << "Third matrix: " << std::endl << m3 << std::endl;
+    if (m2 == m3){
+        std::cout << "Matrix two and three are equal." << std::endl;
+    }
+    if (m2 != m){
+        std::cout << "Matrix one and two are not equal." << std::endl;
+    }
+    m2 += m3;
+    std::cout << "Sum of matrix two and three is: " << std::endl << m2 << std::endl;
+}
+
+void OneElementTest(){
+    Matrix m(1, 1);
+    Matrix m2(1, 1);
+    assert(m.getRow() == 1);
+    assert(m.getCol() == 1);
+    m[0][0] = 7;
+    assert(m[0][0] == 7);
+    m *= 3;
+    assert(m[0][0] == 21);
+    m += m2;
+    assert(m[0][0] == 22);
+    std::cout<< "One element matrix:  "<< m << std::endl;
+}
+
+void OutOfRangeTest1(){
+    Matrix m(3, 3);
+    Matrix m2(5, 3);
+    m += m2;
+    //double x = m[5][2];
+}
+
+void OutOfRangeTest2(){
+    Matrix m(3, 3);
+    double x = m[5][2];
+}
+
 int main(int argc, char* argv[])
 {
 	try{
-        const size_t rows = 5;
-        const size_t cols = 3;
-        Matrix m(rows, cols);
-        assert(m.getRow() == 5);
-        assert(m.getCol() == 3);
-        m[1][2] = 5; // строка 1, колонка 2
-        double x = m[4][1];
-        m *= 3; // умножение на число
-        Matrix m1(rows, cols);
-        if (m1 == m){
-        }
-        m1 += m;
-        std::cout << m1 << std::endl;
-    }
-    catch(std::out_of_range&){
-        std::cout<<"Out of range\n";
+        AllFunctionsTest();
+        OneElementTest();
+        OutOfRangeTest1();
     }
     catch(const char* s){
         std::cout<<s<<std::endl;
+        try{
+            OutOfRangeTest2();
+        }
+        catch(std::out_of_range&){
+            std::cout << "Out of range!\n";
+        }
     }
+    std::cout << "Success!\n";
 	return 0;
 }
